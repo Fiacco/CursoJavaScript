246d296bf2f9fd0074b27649b5896d1b60f6e528
FERNANDO FIACCO <fernando.fiacco@senacsp.edu.br>
2020-03-16 12:25:10 -0300

    exercicios

---
+++ Aula07/js/script02.js
@@ -0,0 +1,15 @@
+function formulario() {
+  var nome = document.createElement("input");
+  var email = document.createElement("input");
+  var botao = document.createElement("input");
+
+  nome.setAttribute("placeholder", "nome");
+  email.setAttribute("type", "email");
+  email.setAttribute("placeholder", "email");
+  botao.nome.setAttribute("type", "submit");
+  botao.nome.setAttribute("value", "enviar dados");
+
+  document.getElementById("nome").append;
+  document.getElementById("email").append;
+  document.getElementById("botao").append;
+}
